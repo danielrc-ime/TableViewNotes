6fdfa1a67ee6364871742cbc8d4bba377e059d90
daniel_master <daniel.rodriguez@master.com.mx>
2020-09-03 00:01:39 -0500

    Aplicando extensiones

--- TableViewNotes/ViewController.swift
+++ TableViewNotes/ViewController.swift
@@ -8,24 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
     var teams: [String] = []
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-          return teams.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "mycell")
-        cell.textLabel?.text  = teams[indexPath.row]
-        
-        cell.imageView!.image = UIImage(named: teams[indexPath.row])!
-        return cell
-    }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +20,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         
         teams = ["Atletico de Madrid", "Barcelona", "Deportivo de la Coruña", "Las Palmas", "Malaga", "Rayo Vallecano", "Sporting", "Real Sociedad", "Espanyol", "Mallorca", "Valladolid", "Eibar",  "Ponferradina", "Albacete"]
-
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return teams.count
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "mycell")
+        cell.textLabel?.text  = teams[indexPath.row]
+        
+        cell.imageView!.image = UIImage(named: teams[indexPath.row])!
+        return cell
+    }
+}
